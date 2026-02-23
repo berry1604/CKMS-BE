d5005ec3843b1e0d92a7a5a069be736fc3d6708f
299410 <299410@gmail.com>
2026-02-23 11:09:06 +0700

    get my store order

--- src/main/java/com/swp/ckms/service/StoreOrderService.java
+++ src/main/java/com/swp/ckms/service/StoreOrderService.java
@@ -3,6 +3,11 @@ package com.swp.ckms.service;
 import com.swp.ckms.dto.request.StoreOrderRequest;
 import com.swp.ckms.dto.response.StoreOrderResponse;
 
+import com.swp.ckms.entity.OrderStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface StoreOrderService {
     StoreOrderResponse createOrder(StoreOrderRequest request, String username);
+    Page<StoreOrderResponse> getMyOrders(String username, OrderStatus status, Pageable pageable);
 }
