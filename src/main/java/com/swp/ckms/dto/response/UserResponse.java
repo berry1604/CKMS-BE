56b8159c8786c041dae184a56c83126094621763
vominhluan17 <luanvmse182794@fpt.edu.vn>
2026-02-27 23:26:51 +0700

    get user

---
+++ src/main/java/com/swp/ckms/dto/response/UserResponse.java
@@ -0,0 +1,20 @@
+package com.swp.ckms.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetUserResponse {
+
+    private Long userId;
+    private String username;
+    private String email;
+    private String fullName;
+    private String role;
+    private String status;
+    private Boolean isActive;
+
+}
---
+++ src/main/java/com/swp/ckms/specification/UserSpecification.java
@@ -0,0 +1,4 @@
+package com.swp.ckms.specification;
+
+public class UserSpecification {
+}
